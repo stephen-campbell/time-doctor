f009fcf69583f72955b34894d64c2c6163fb5473
stephen-campbell <swcampbell1@gmail.com>
2020-02-13 20:47:55 -0800

    Updates

---
+++ precache-manifest.84c5745bb51c0b153a28f6636726ff6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d468de371d73862b08981d0a29c2b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "721a5d796ca8aaecc592",
+    "url": "/static/css/2.1b53b9c3.chunk.css"
+  },
+  {
+    "revision": "645c05008f0de6073c47",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "721a5d796ca8aaecc592",
+    "url": "/static/js/2.a61cf717.chunk.js"
+  },
+  {
+    "revision": "46c482ef0575d001a3428c51daa7f5e8",
+    "url": "/static/js/2.a61cf717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645c05008f0de6073c47",
+    "url": "/static/js/main.10ec208f.chunk.js"
+  },
+  {
+    "revision": "adf60aa4bf41988e7a47",
+    "url": "/static/js/runtime-main.9ffb3d8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
