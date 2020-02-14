17742d3552079402ea6634d64f3a4856dbda70f2
stephen-campbell <swcampbell1@gmail.com>
2020-02-13 18:55:32 -0800

    functionality works

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import logo from "./logo.svg";
 import Time from "./Time";
 import "./App.css";
+import moment from "moment";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Time></Time>
+        {/* <Time targetTime={new Date().getTime() + 86400 * 10000}></Time> */}
+        <Time
+          targetTime={moment()
+            .add(23, "hours")
+            .format()}
+          includeTime={false}
+        ></Time>
       </header>
     </div>
   );
--- src/Time.js
+++ src/Time.js
@@ -1,19 +1,53 @@
 import React from "react";
+import Moment from "react-moment";
+import moment from "moment";
 
 class Time extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentTime: new Date().toLocaleString()
+      currentTime: moment().format()
+      //   displayTimeTwo: this.props.includeTime
+      //     ? moment(moment().format()).diff(this.props.targetTime, "seconds") >= 1
+      //     : moment(moment().format()).diff(this.props.targetTime, "days") >= 1
     };
   }
   //   changeColor = () => {
   //     this.setState({ color: "blue" });
   //   };
   render() {
+    console.log("state", this.state);
+    const displayTimeTwo = this.props.includeTime
+      ? Math.abs(
+          moment(moment().format()).diff(this.props.targetTime, "seconds")
+        ) >= 1
+      : // ? "greater than 1 second"
+        // : "less than one second"
+        Math.abs(
+          moment(moment().format()).diff(this.props.targetTime, "days")
+        ) >= 1;
+    //   ? "greater than 1 day"
+    //   : "less than 1 day";
     return (
       <div>
-        <p>{this.state.currentTime}</p>
+        <p>display second: {displayTimeTwo.toString()}</p>
+        <p>
+          Current time <Moment date={this.state.currentTime} />
+        </p>
+        {displayTimeTwo ? (
+          <p>
+            TargetTime <Moment date={this.props.targetTime} />
+          </p>
+        ) : (
+          <p> </p>
+        )}
+        <p>
+          Difference:{" "}
+          <Moment diff={this.state.currentTime} unit="hours" decimal>
+            {this.props.targetTime}
+          </Moment>{" "}
+          hours
+        </p>
         {/* <button type="button" onClick={this.changeColor}>
           Change color
         </button> */}
